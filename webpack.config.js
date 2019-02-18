73fd48126109fe57bdcd869bb88954e761846fec
Greene <jungle1900@gmail.com>
2019-02-18 19:59:05 +0800

    remove copy-webpack-plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -32,7 +31,7 @@ module.exports = {
         use: {
           loader: 'file-loader'
         }
-      }
+      },
     ]
   },
   plugins: [
@@ -40,10 +39,5 @@ module.exports = {
       template: 'src/index.html'
     }),
     new CleanWebpackPlugin('dist'),
-    new CopyWebpackPlugin([
-      { from: 'assets', to: '' },
-    ], {
-      ignore: ['.DS_Store']
-    }),
   ]
 }
