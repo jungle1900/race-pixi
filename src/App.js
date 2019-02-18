e14b3ef45cf997b7dcd85ddf3eb6fd506c1d7330
Greene <jungle1900@gmail.com>
2019-02-18 19:54:16 +0800

    add file loader for images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js'
+import bunnyUrl from '../assets/bunny.png'
 
 export default class {
   constructor(target, number = 1) {
@@ -6,12 +7,12 @@ export default class {
     this.number = number
 
     this.app = new PIXI.Application({backgroundColor : 0x1099bb})
-    this.texture = PIXI.Texture.fromImage('bunny.png')
+    this.bunnyTex = PIXI.Texture.fromImage(bunnyUrl)
   }
 
   run() {
     for (let i = 0; i < this.number; i += 1) {
-      let bunny = new PIXI.Sprite(this.texture)
+      let bunny = new PIXI.Sprite(this.bunnyTex)
       bunny.anchor.set(0.5)
       bunny.x = Math.floor(Math.random() * this.app.screen.width)
       bunny.y = Math.floor(Math.random() * this.app.screen.height)
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,12 @@ module.exports = {
           }
         }
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
     ]
   },
   plugins: [
