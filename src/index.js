56da79b73f6297c16172dc2e89c3f95656d0ee9c
Greene <jungle1900@gmail.com>
2019-02-18 17:34:11 +0800

    export from index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import App from './App.js'
+import * as PIXI from 'pixi.js'
 
-const app = new App(document.getElementById('game'), 100)
-app.run()
+export default class {
+  constructor(target, number = 1) {
+    this.target = target
+    this.number = number
+
+    this.app = new PIXI.Application({backgroundColor : 0x1099bb})
+    this.texture = PIXI.Texture.fromImage('assets/bunny.png')
+  }
+
+  run() {
+    for (let i = 0; i < this.number; i += 1) {
+      let bunny = new PIXI.Sprite(this.texture)
+      bunny.anchor.set(0.5)
+      bunny.x = Math.floor(Math.random() * this.app.screen.width)
+      bunny.y = Math.floor(Math.random() * this.app.screen.height)
+
+      this.app.stage.addChild(bunny);
+    }
+
+    this.target.appendChild(this.app.view)
+  }
+}
